64ecacf9bc2bc5c27aa8b659160aa5b1b8403225
devkyoung2 <devkyoung2@gmail.com>
2025-02-11 15:20:36 +0900

    [feat] And 웹뷰 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,40 @@
-import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { BackHandler, Alert } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
-  const dismissKeyboard = () => {
-    Keyboard.dismiss();
+  const webViewRef = useRef<WebView>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const handleBackPress = () => {
+    if (!webViewRef.current) return;
+
+    if (canGoBack) {
+      webViewRef.current.goBack();
+      return true;
+    } else {
+      Alert.alert('알림', '앱을 종료하시겠습니까?', [
+        { text: '취소', style: 'cancel' },
+        { text: '종료', onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    }
   };
 
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <TouchableWithoutFeedback onPress={dismissKeyboard}>
-        <View style={styles.container}>
-          <WebView
-            // source={{ uri: 'http://localhost:5173' }}
-            source={{ uri: 'https://bbaegok.store' }}
-            style={styles.webView}
-            sharedCookiesEnabled={true}
-            hideKeyboardAccessoryView={true}
-            showsVerticalScrollIndicator={false}
-            overScrollMode='never' // 새로고침 방지
-            bounces={false} // iOS에서 흔들림 방지
-            scrollEnabled={false} // 스크롤 비활성화
-            keyboardDisplayRequiresUserAction={false}
-          />
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <WebView
+      ref={webViewRef}
+      userAgent='Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36'
+      source={{ uri: 'https://bbaegok.store' }}
+      style={{ flex: 1 }}
+      overScrollMode='never'
+      onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  webView: {
-    flex: 1,
-  },
-});
