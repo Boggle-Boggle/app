6691ec111bdb49b191d6b91507526b390b24a57b
devkyoung2 <devkyoung2@gmail.com>
2025-02-22 02:47:32 +0900

    [feat] IOS 웹뷰 설계 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,82 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { BackHandler, Alert, View, StatusBar } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Keyboard, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
-  const webViewRef = useRef<WebView>(null);
-  const [canGoBack, setCanGoBack] = useState(false);
-  const [currentUrl, setCurrentUrl] = useState<string>('#FFFFFF');
-  const getStatusColor = () => {
-    if (currentUrl.includes('login')) return '#CBBAB9';
-    if (currentUrl.includes('note')) return '#DCD7D6';
-    if (
-      currentUrl.includes('signup') ||
-      currentUrl.includes('signUp') ||
-      currentUrl.includes('nickname') ||
-      currentUrl.includes('terms') ||
-      currentUrl.includes('VersionInfo') ||
-      currentUrl.includes('deleteAccount') ||
-      currentUrl.includes('edit')
-    )
-      return '#FFFFFF';
+  const webViewRef = useRef(null);
 
-    return '#EEEDEB';
-  };
-
-  const handleBackPress = () => {
-    if (!webViewRef.current) return;
-
-    if (canGoBack) {
-      webViewRef.current.goBack();
-      return true;
-    } else {
-      Alert.alert('알림', '앱을 종료하시겠습니까?', [
-        { text: '취소', style: 'cancel' },
-        { text: '종료', onPress: () => BackHandler.exitApp() },
-      ]);
-      return true;
-    }
-  };
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState('');
 
   useEffect(() => {
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    const showListener = Keyboard.addListener('keyboardDidShow', () => setKeyboardVisible(true));
+    const hideListener = Keyboard.addListener('keyboardDidHide', () => setKeyboardVisible(false));
+    console.log('Dd');
 
-    return () => backHandler.remove();
-  }, [canGoBack]);
+    return () => {
+      showListener.remove();
+      hideListener.remove();
+    };
+  }, []);
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const canSwipe = (url: string) => {
+    const baseDomain = 'https://bbaegok.store';
+
+    const noSwipePatterns = [
+      baseDomain + '/$',
+      baseDomain + '/search$',
+      baseDomain + '/library$',
+      baseDomain + '/myPage$',
+    ];
+
+    const allowSwipePatterns = [baseDomain + '/search\\?.*', baseDomain + '/myPage/.*'];
+
+    if (allowSwipePatterns.some((pattern) => new RegExp(pattern).test(url))) return true;
+    if (noSwipePatterns.some((pattern) => new RegExp(pattern).test(url))) return false;
+
+    return true;
+  };
+
+  const isKeyboardAvoidingPage = () => {
+    if (currentUrl.includes('nickname')) return 'height';
+
+    return undefined;
+  };
 
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar backgroundColor={getStatusColor()} barStyle='dark-content' />
-      <WebView
-        ref={webViewRef}
-        userAgent='Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36'
-        source={{ uri: 'https://bbaegok.store' }}
-        style={{ flex: 1 }}
-        overScrollMode='never'
-        onNavigationStateChange={(navState) => {
-          setCanGoBack(navState.canGoBack);
-          setCurrentUrl(navState.url);
-        }}
-      />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior={isKeyboardAvoidingPage()}>
+      <TouchableWithoutFeedback onPressIn={dismissKeyboard}>
+        <WebView
+          ref={webViewRef}
+          source={{ uri: 'https://bbaegok.store' }}
+          style={[styles.webView]}
+          bounces={false}
+          scrollEnabled={!keyboardVisible}
+          hideKeyboardAccessoryView={true}
+          showsVerticalScrollIndicator={false}
+          overScrollMode='never'
+          contentInsetAdjustmentBehavior='never'
+          keyboardDisplayRequiresUserAction={true}
+          onNavigationStateChange={(event) => {
+            setCurrentUrl(event.url);
+          }}
+          allowsBackForwardNavigationGestures={canSwipe(currentUrl)}
+        />
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: 'pink',
+  },
+  webView: {
+    flex: 1,
+  },
+});
